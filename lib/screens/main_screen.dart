fa15da812da51e0a98cd6a0aa94eafc6945d578b
Martin <dechevmartin@gmail.com>
2021-08-25 00:43:26 +0300

    use local_image package to fetch local images

--- lib/screens/main_screen.dart
+++ lib/screens/main_screen.dart
@@ -1,8 +1,13 @@
+import 'dart:math';
+
 import 'package:badges/badges.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:local_image_provider/device_image.dart';
+import 'package:local_image_provider/local_image.dart';
 import 'package:picpick/utils/constants.dart';
 import 'package:positioned_tap_detector/positioned_tap_detector.dart';
+import 'package:local_image_provider/local_image_provider.dart' as lip;
 
 const dummyImage = 'images/profile.jpeg';
 
@@ -15,9 +20,10 @@ class MainScreen extends StatefulWidget {
 
 class _MainScreenState extends State<MainScreen> {
   final GlobalKey _imageKey = GlobalKey();
-
+  List<LocalImage> _loadedImages;
   Size imageSize;
   int _numItemsDeleted = 0;
+  bool _updatePicture = false;
 
   getSizeAndPosition() {
     RenderBox _imageBox = _imageKey.currentContext.findRenderObject();
@@ -43,10 +49,7 @@ class _MainScreenState extends State<MainScreen> {
                 borderRadius: BorderRadius.all(
                   Radius.circular(20),
                 ),
-                child: Image.asset(
-                  dummyImage,
-                  fit: BoxFit.contain,
-                ),
+                child: _getCurrentImage(),
               ),
               onTap: (position) => _printTap('Single tap', position),
               onDoubleTap: (position) => _printTap('Double tap', position),
@@ -98,11 +101,60 @@ class _MainScreenState extends State<MainScreen> {
     );
   }
 
+  Widget _getCurrentImage() {
+    return _loadedImages == null
+        ? Image.asset(
+            dummyImage,
+            fit: BoxFit.contain,
+          )
+        : _loadedImages.isEmpty
+            ? Image.asset(
+                dummyImage,
+                fit: BoxFit.contain,
+              )
+            : Image(
+                image: DeviceImage(_loadedImages[_getRandomPictureIndex()]),
+                fit: BoxFit.contain,
+              );
+  }
+
+  int _getRandomPictureIndex() {
+    var rng = new Random();
+
+    if (_loadedImages == null) {
+      print("error, shouldn't get here with an empty images");
+      return 0;
+    }
+
+    if (_loadedImages.isEmpty) {
+      print("error, for some reason the images are empty");
+      return 0;
+    }
+
+    return rng.nextInt(_loadedImages.length);
+  }
+
+  Future<void> handleImages() async {
+    lip.LocalImageProvider imageProvider = lip.LocalImageProvider();
+    bool hasPermission = await imageProvider.initialize();
+    if (hasPermission) {
+      List<LocalImage> images = await imageProvider.findLatest(10);
+      setState(() {
+        _loadedImages = images;
+      });
+      print('got here, images count: ${images.length}');
+      images.forEach((image) => print("The image id is: ${image.id}"));
+    } else {
+      print("The user has denied access to images on their device.");
+    }
+  }
+
   void _printTap(String gesture, TapPosition position) {
     getSizeAndPosition();
     double middleX = imageSize.width / 2;
     if (position.relative.dx > middleX) {
       print('right click');
+      handleImages().onError((error, stackTrace) => {print("error")});
     } else {
       print('left click');
       setState(() {
--- lib/screens/welcome_screen.dart
+++ lib/screens/welcome_screen.dart
@@ -1,9 +1,24 @@
 import 'package:flutter/material.dart';
 import 'package:picpick/screens/main_screen.dart';
-import 'package:picpick/utils/constants.dart';
+//import 'package:photo_manager/photo_manager.dart';
 
 class WelcomeScreen extends StatelessWidget {
   static const RouteKey = '/welcome_screen';
+
+//  @override
+//  void initState() {
+//    PhotoManager.requestPermissionExtend().then((result) => {
+//          if (result.isAuth)
+//            {
+//              // success
+//            }
+//          else
+//            {
+//              // fail
+//              /// if result is fail, you can call `PhotoManager.openSetting();`  to open android/ios applicaton's setting to get permission
+//            }
+//        });
+//  }
 
   @override
   Widget build(BuildContext context) {
